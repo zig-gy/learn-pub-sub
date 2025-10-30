0e9fae15d4725a85ae5b1e68fea831cb83b7fb0e
zig-gy <benjamin.f.chadwick@proton.me>
2025-10-29 23:11:55 -0300

    Format import statements in main.go for consistency

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	name, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Printf("error reading username, error: %e", err)
+		return
+	}
+
 }
